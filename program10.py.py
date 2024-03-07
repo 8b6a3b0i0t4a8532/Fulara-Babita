f165683192781db4131dd004fce57f5d43a71334
8b6a3b0i0t4a8532 <160332405+8b6a3b0i0t4a8532@users.noreply.github.com>
2024-03-07 21:07:09 +0530

    Add files via upload

---
+++ program10.py.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# write a program to check a string is pallidrome 0r not
+s = "malayalam"
+ns = s[::-1]
+
+if s==ns:
+    print("pallindrome")
+else:
+    print("not")
+
+
+# In[ ]:
+
+
+
+
---
+++ program11.py.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+#Python program to generate Fibonacci series until nth term using while loop
+n = int(input("Enter the value of nth term: ")) 
+a = 0 
+b = 1 
+c = 0 
+count = 1 
+print("Fibonacci Series: ", end = " ") 
+while(count <= n):
+    print(c, end = " ") 
+    count += 1 
+    a = b 
+    b = sum 
+    sum = a + b
+
+
+# In[ ]:
+
+
+
+
